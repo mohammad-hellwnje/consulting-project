758ed6a34dd906e5fb979dd2e529183c5056d429
joudy swied <eng.joudyswied@gmail.com>
2025-05-08 14:36:24 -0700

    version 1

---
+++ src/Data/NavData.tsx
@@ -0,0 +1,27 @@
+export const NavData = [
+  {
+    name: "الرئيسية",
+    path: "/",
+  },
+  {
+    name: "من نحن",
+    path: "/careers",
+  },
+  {
+    name: "كيف نعمل",
+    path: "/about",
+  },
+  {
+    name: "  خدماتنا",
+    path: "/security",
+  },
+  {
+    name: "الأسئلة الشائعة",
+    path: "/security",
+  },
+
+  {
+    name: "تواصل معي",
+    path: "/security",
+  },
+];
